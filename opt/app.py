4bb77463aa4201c67458a08b98c9a47816c72751
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 01:55:50 +0900

    init slack

--- opt/app.py
+++ opt/app.py
@@ -1,3 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
+
+# slack初期化
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+openai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
