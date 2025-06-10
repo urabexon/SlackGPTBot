0423d9a0b4df600628d0ed6da3103c33624fe9b2
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 23:02:05 +0900

    gpt4o exec init func setup

---
+++ opt/gpt_4o.py
@@ -0,0 +1,13 @@
+from util import get_history_identifier, get_user_identifier, calculate_num_tokens, calculate_num_tokens_by_prompt, say_ts, check_availability
+from typing import List, Dict
+import os
+
+class GPT_4O_CommandExecutor():
+    """GPT-4oを使って添付画像を含めて会話をするコマンドの実行クラス"""
+
+    def __init__(self, client_openai):
+
+    def execute(self, client, message, say, context, logger):
+    
+    def execute_reset(self, client, message, say, context, logger):
+        """GPT-4oを使って会話履歴のリセットをするコマンドの実行メソッド"""
