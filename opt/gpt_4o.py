ea090c85424e79d72e0c49b9ad959c5e8b7c3454
urabexon <hiroki708.a48uk@gmail.com>
2025-06-11 01:18:04 +0900

    call log info debug

--- opt/gpt_4o.py
+++ opt/gpt_4o.py
@@ -73,8 +73,14 @@ class GPT_4O_CommandExecutor():
             return
 
         say_ts(client, message, f"GPT-4oで <@{message['user']}> さんの以下の発言に対応中（履歴数: {len(history_array)} 、トークン数: {calculate_num_tokens(history_array)}）\n```\n{prompt}\n```")
+
         # ChatCompletionを呼び出す
-        
+        logger.info(f"user: {message['user']}, prompt: {prompt}")
+        logger.debug(response)
+
+        # ヒストリー新追加
+
+        # トークンのサイズがINPUT_MAX_TOKEN_SIZEを超えたら古いものを削除
     
     def execute_reset(self, client, message, say, context, logger):
         """GPT-4oを使って会話履歴のリセットをするコマンドの実行メソッド"""
