5d7ccc589ea7f9438bf8a4bd18cf69fef04e75d5
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 23:09:42 +0900

    token size setup

--- opt/gpt_4o.py
+++ opt/gpt_4o.py
@@ -5,6 +5,10 @@ import os
 class GPT_4O_CommandExecutor():
     """GPT-4oを使って添付画像を含めて会話をするコマンドの実行クラス"""
 
+    MAX_TOKEN_SIZE = 128000  # トークンの最大サイズ
+    COMPLETION_MAX_TOKEN_SIZE = 2048  # ChatCompletionの出力の最大トークンサイズ
+    INPUT_MAX_TOKEN_SIZE = MAX_TOKEN_SIZE - COMPLETION_MAX_TOKEN_SIZE  # ChatCompletionの入力に使うトークンサイズ
+
     def __init__(self, client_openai):
 
     def execute(self, client, message, say, context, logger):
