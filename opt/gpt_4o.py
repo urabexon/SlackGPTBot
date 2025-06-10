b05a41a1ccad6a4d1a475d14b32dced23cdcc6e0
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 23:42:20 +0900

    import requests base64 file in message

--- opt/gpt_4o.py
+++ opt/gpt_4o.py
@@ -1,6 +1,8 @@
 from util import get_history_identifier, get_user_identifier, calculate_num_tokens, calculate_num_tokens_by_prompt, say_ts, check_availability
 from typing import List, Dict
 import os
+import requests
+import base64
 
 class GPT_4O_CommandExecutor():
     """GPT-4oを使って添付画像を含めて会話をするコマンドの実行クラス"""
@@ -34,7 +36,10 @@ class GPT_4O_CommandExecutor():
         ]
 
         if "files" in message:
-
+            for file in message["files"]:
+                url = file["url_private"]
+                SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+                
     
     def execute_reset(self, client, message, say, context, logger):
         """GPT-4oを使って会話履歴のリセットをするコマンドの実行メソッド"""
