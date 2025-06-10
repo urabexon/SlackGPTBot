804ba1cfc322e124ab327c6e5534221c21cdd028
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 20:54:59 +0900

    gpt4exec init func setup

---
+++ opt/gpt_4.py
@@ -0,0 +1,11 @@
+from util import get_history_identifier, get_user_identifier, calculate_num_tokens, calculate_num_tokens_by_prompt, say_ts, check_availability
+from typing import List, Dict
+
+class GPT_4_CommandExecutor():
+    """GPT-4を使って会話をするコマンドの実行クラス"""
+
+    def __init__(self, client_openai):
+
+    def execute(self, client, message, say, context, logger):
+    
+    def execute_reset(self, client, message, say, context, logger):
