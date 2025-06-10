ff346aea72f709b64fd8fd0127586d226de737e2
urabexon <hiroki708.a48uk@gmail.com>
2025-06-10 22:22:02 +0900

    append new history

--- opt/gpt_4.py
+++ opt/gpt_4.py
@@ -32,7 +32,7 @@ class GPT_4_CommandExecutor():
             history_array = history_array[1:]
 
         # 単一の発言でMAX_TOKEN_SIZEを超えたら対応できない
-         if(len(history_array) == 0):
+        if(len(history_array) == 0):
             messege_out_of_token_size = f"発言内容のトークン数が{self.INPUT_MAX_TOKEN_SIZE}を超えて、{calculate_num_tokens_by_prompt(prompt)}であったため、対応できませんでした。"
             say_ts(client, message, messege_out_of_token_size)
             logger.info(messege_out_of_token_size)
@@ -45,6 +45,9 @@ class GPT_4_CommandExecutor():
 
 
         # ヒストリー新追加
+        new_response_message = response.choices[0].message
+        history_array.append(new_response_message)
+
         # トークンのサイズがINPUT_MAX_TOKEN_SIZEを超えたら古いものを削除
     
     def execute_reset(self, client, message, say, context, logger):
