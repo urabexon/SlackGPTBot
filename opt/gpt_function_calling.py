30835967aed0d24a3ab75cbad3d26576d8a5e79d
urabexon <hiroki708.a48uk@gmail.com>
2025-06-11 13:10:32 +0900

    token size set

--- opt/gpt_function_calling.py
+++ opt/gpt_function_calling.py
@@ -4,3 +4,14 @@ import os
 
 class GPT_Function_Calling_CommandExecutor():
     """ChatGPT Function Calling を使ってWeb検索利用やSlack検索利用の会話をするコマンドの実行クラス"""
+
+    FUNCTIONS = []
+
+    MAX_TOKEN_SIZE = 128000  # トークンの最大サイズ
+    COMPLETION_MAX_TOKEN_SIZE = 4096  # ChatCompletionの出力の最大トークンサイズ
+    # ChatCompletionの入力に使うトークンサイズ、FUNCTION分はJSON化してプロンプトとして雑に計算する(トークン計算方法不明のため)
+    INPUT_MAX_TOKEN_SIZE = MAX_TOKEN_SIZE - \ 
+        COMPLETION_MAX_TOKEN_SIZE - \
+        calculate_num_tokens_by_prompt(json.dumps(FUNCTIONS, ensure_ascii=False))
+    
+
