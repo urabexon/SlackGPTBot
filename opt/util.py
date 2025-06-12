dec432904a8f55b7c22995bde4ee6aba8582f3db
urabexon <hiroki708.a48uk@gmail.com>
2025-06-12 21:27:44 +0900

    get user id

--- opt/util.py
+++ opt/util.py
@@ -53,9 +53,11 @@ def get_history_identifier(team, channel, user):
 
 def get_user_identifier(team, user):
     """ユーザーを特定するためのIDを生成する"""
+    return f"slack-{team}-{user}"
 
 def check_availability(message, logger) -> bool:
     """このチャンネルが利用可能かどうかを返す。 check True で利用可能"""
+    
 
 def check_daily_user_limit(message, usage_log) -> bool:
     """このユーザーが利用可能かどうかを返す。 check True で利用可能"""
