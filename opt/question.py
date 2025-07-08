9904ce7e0256ced88955e40976078fd53395a7f8
urabexon <hiroki708.a48uk@gmail.com>
2025-07-08 12:55:52 +0900

    info query check

--- opt/question.py
+++ opt/question.py
@@ -5,10 +5,7 @@ import os
 import re
 
 from dotenv import load_dotenv
-
 load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 MAX_TOKEN_SIZE = 128000
 COMPLETION_MAX_TOKEN_SIZE = 4096
@@ -16,5 +13,16 @@ INPUT_MAX_TOKEN_SIZE = MAX_TOKEN_SIZE - COMPLETION_MAX_TOKEN_SIZE
 
 def say_question(client_openai, client, message, say, using_user, target_channel, logger):
     """
-    
+    質問の答えのメッセージを送信する
     """
+
+    logger.info(f"<@{using_user}>  さんの以下の質問にパブリックチャンネルの検索結果を踏まえて対応中\n```\n{question}\n```")
+    say_ts(client, message, f"<@{using_user}>  さんの以下の質問にパブリックチャンネルの検索結果を踏まえて対応中\n```\n{question}\n```")
+
+    # ChatCompletionから適切なクエリを聞く
+    query_ask_prompt = f"「{question}」という質問をSlackの検索で調べるときに適切な検索クエリを教えてください。検索クエリとは単一の検索のための単語、または、複数の検索のための単語を半角スペースで繋げた文字列です。検索クエリを##########検索クエリ##########の形式で教えてください。"
+    query_gpt_response = client_openai.chat.completions.create()
+
+    logger.debug(chat_gpt_response)
+
+    say_ts(client, message, chat_gpt_response.choices[0].message.content)
